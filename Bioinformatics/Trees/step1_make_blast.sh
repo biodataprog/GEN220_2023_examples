a5a341b4bd286248078aac7d59156394c8ae210e
Jason Stajich <jasonstajich.phd@gmail.com>
2023-11-22 17:21:52 -0800

    add more example ouput

--- Bioinformatics/Trees/step1_make_blast.sh
+++ Bioinformatics/Trees/step1_make_blast.sh
@@ -3,12 +3,12 @@
 
 module load ncbi-blast
 # run indexing if database is not indexed
-if [ ! -f all_db.fasta.psq ]; then 
-	makeblastdb -in all_db.fasta -dbtype prot
+if [ ! -f uniprot_sprot.fasta.psq ]; then 
+	makeblastdb -in uniprot_sprot.fasta -dbtype prot -parse_seqids
 fi
 
 QUERY=MET12.fa
 BASE=$(basename $QUERY .fa)
-OUTPUT=${BASE}_vs_db.BLASTP
+OUTPUT=${BASE}_vs_sprot.BLASTP
 
-blastp -query $QUERY -db all_db.fasta -evalue 1e-20 -out $OUTPUT -outfmt 6
+blastp -query $QUERY -db uniprot_sprot.fasta -evalue 1e-20 -out $OUTPUT -outfmt 6
--- Bioinformatics/Trees/step2_make_aln.sh
+++ Bioinformatics/Trees/step2_make_aln.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/bash
 #SBATCH --nodes 1 --ntasks 2 --mem 2G
 
-module load cdbfasta
+module load ncbi-blast
 module load muscle
 module load trimal
 module load fasttree
 
-# run indexing if database is not indexed
-if [ ! -f all_db.fasta.cidx ]; then 
-	cdbfasta all_db.fasta
-fi
-
 QUERY=MET12.fa
 BASE=$(basename $QUERY .fa)
-BLASTOUTPUT=${BASE}_vs_db.BLASTP
+BLASTOUTPUT=${BASE}_vs_sprot.BLASTP
 
-cut -f2 $BLASTOUTPUT | uniq | cdbyank all_db.fasta.cidx > $BASE.hit_seqs.fasta
-
-muscle -in $BASE.hit_seqs.fasta -out $BASE.hit_seqs.fasaln
-muscle -in $BASE.hit_seqs.fasta -out $BASE.hit_seqs.aln -clw
+cut -f2 $BLASTOUTPUT | uniq > MET12.hits.txt
+ 
+blastdbcmd -entry_batch MET12.hits.txt -db uniprot_sprot.fasta > $BASE.hit_seqs.fasta
+perl -i -p -e 's/>(\S+)\s+(.+)OS=(\S+)\s+(\S+)/>$1_$3_$4 $2/' MET12.hit_seqs.fasta
+muscle -align $BASE.hit_seqs.fasta -output $BASE.hit_seqs.fasaln
 
 trimal -in $BASE.hit_seqs.fasaln -out $BASE.hit_seqs.trim -automated1
 trimal -in $BASE.hit_seqs.fasaln -out $BASE.hit_seqs.trim_aln -automated1 -clustal
