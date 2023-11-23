463403693ab07a408ef9794904c8f5858d38fc9d
Jason Stajich <jasonstajich.phd@gmail.com>
2023-11-22 16:20:18 -0800

    add extraction step

--- Bioinformatics/Protein_domains/hmmer_pkinase.sh
+++ Bioinformatics/Protein_domains/hmmer_pkinase.sh
@@ -9,3 +9,10 @@ hmmsearch --cut_ga --domtbl Sacch_Kinases.hmmsearch.domtbl Pkinase.hmm Saccharom
 
 echo 'Now we can see some of the hits'
 grep -v ^# Sacch_Kinases.hmmsearch.domtbl | awk '{print $1,$4,$7}' 
+
+# retrieve the sequences that are hits, based on their ID
+esl-sfetch -f Saccharomyces_cerevisiae.peps.fa Pkinase_hits.txt > Pkinase_hits.fa
+
+# alternatively you can do it this way:
+# module load samtools
+# samtools faidx -r Pkinase_hits.txt Saccharomyces_cerevisiae.peps.fa > Pkinase_hits.fa
