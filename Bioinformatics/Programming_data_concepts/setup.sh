2a458fc1337af46353ad58372072667da8347b63
Jason Stajich <jasonstajich.phd@gmail.com>
2023-12-01 15:57:34 -0800

    copy

--- Bioinformatics/Programming_data_concepts/setup.sh
+++ Bioinformatics/Programming_data_concepts/setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash -l
 
 cp /bigdata/gen220/shared/data-examples/examples/rice_all_annotation.gff3.gz .
+
+cp /bigdata/gen220/shared/data-examples/examples/S_enterica.pep.gz  .
+cp /bigdata/gen220/shared/data-examples/examples/CDS.fa .
